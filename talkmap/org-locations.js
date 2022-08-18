98721e47808e29dbeb38ae906ae06d077b8c9318
bertjiazheng <jiajia@qunhemail.com>
2022-08-18 13:03:11 +0800

    add VALSE 2022

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,13 +1,18 @@
 var addressPoints = [
   [
-    "Xi'an, China",
-    34.220444,
-    109.11150598002203
+    "Tianjin, China",
+    39.0856735,
+    117.1951073
   ],
   [
     "Hangzhou, China",
     30.2489634,
     120.2052342
+  ],
+  [
+    "Xi'an, China",
+    34.3430507,
+    108.9350839
   ],
   [
     "Hefei, China",
